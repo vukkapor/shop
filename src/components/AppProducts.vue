bb6de60563c948b76496e10e0e560e52b58579b1
Vuk Kapor <vukkapor12@gmail.com>
2019-06-28 21:17:02 +0200

    product search

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
+    Pretraga proizvoda:
+    <input type="text" v-model="search">
     <ul>
-      <li v-for="(product, index) in products" :key="index">
+      <li v-for="(product, index) in filteredProducts" :key="index">
         <SingleProduct :product="product" @delete-product="deleteProduct(product)"/>
       </li>
     </ul>
@@ -10,16 +12,22 @@
 
 <script>
 import { productService } from "@/services/products";
-import SingleProduct from "@/components/SingleProduct"
+import SingleProduct from "@/components/SingleProduct";
 export default {
   data() {
     return {
-      products: productService.list()
+      products: productService.list(),
+      search: ""
     };
   },
 
   components: {
     SingleProduct
+  },
+  computed: {
+    filteredProducts: function() {
+      return this.products.filter(prod => prod.name.toLowerCase().includes(this.search.toLowerCase()));
+    }
   }
 };
 </script>
