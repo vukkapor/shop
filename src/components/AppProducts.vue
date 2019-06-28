e86fecb6787a465f398df435898769809b7eea96
Vuk Kapor <vukkapor12@gmail.com>
2019-06-28 20:47:05 +0200

    Added products

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,9 +1,25 @@
 <template>
-    
+  <div>
+    <ul>
+      <li v-for="(product, index) in products" :key="index">
+        <SingleProduct :product="product" @delete-product="deleteProduct(product)"/>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { productService } from "@/services/products";
+import SingleProduct from "@/components/SingleProduct"
 export default {
+  data() {
+    return {
+      products: productService.list()
+    };
+  },
 
-}
+  components: {
+    SingleProduct
+  }
+};
 </script>
---
+++ src/components/SingleProduct.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Naziv proizvoda:
+    {{product.name}}
+    <br>
+    Quantity: 
+    {{product.quantity}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["product"],
+};
+</script>
