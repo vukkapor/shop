02b113528efeb7c4bd9482e698d775aa1af9d689
Vuk Kapor <vukkapor12@gmail.com>
2019-06-28 21:23:28 +0200

    product + -

--- src/components/SingleProduct.vue
+++ src/components/SingleProduct.vue
@@ -3,13 +3,25 @@
     Naziv proizvoda:
     {{product.name}}
     <br>
-    Quantity: 
+    Quantity:
     {{product.quantity}}
+    <button type="submit" @click="addProduct">+</button>
+    <button type="submit" @click="removeProduct">-</button>
   </div>
 </template>
 
 <script>
+import { productService } from "@/services/products";
 export default {
   props: ["product"],
+
+  methods: {
+    addProduct() {
+        productService.increment(this.product)
+    },
+    removeProduct() {
+        productService.decrement(this.product)
+    }
+  }
 };
 </script>
